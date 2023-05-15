16ec92ee3916ccb07de63edaf28f01037cbd8022
Brysontech <brightagu2@gmail.com>
2023-05-15 23:41:47 +0100

    update

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,45 @@
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+import connectDb from "@utils/connectDb";
+import User from "@models/user";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+
+  callbacks: {
+    async session({ session }) {
+      //get user data and set it to the session storage
+      const sessionUser = await User.findOne({ email: session.user.id });
+      session.user.id = sessionUser?._id.toString();
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        // this is a setverless function
+        await connectDb;
+
+        //check if the use exists
+        const userExists = await User.findOne({ email: profile.email });
+        if (!userExists) {
+          //create a new user and save it to the database
+          await User.create({
+            email: profile.email,
+            userName: profile.name.replace(" ", "").toString(),
+            image: profile.image,
+          });
+        }
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
---
+++ app/layout.jsx
@@ -0,0 +1,26 @@
+import "@styles/globals.css";
+import Provider from "@components/Provider";
+import Nav from "@components/Nav";
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover and share AI prompt",
+};
+const Layout = ({ children }) => {
+  return (
+    <html>
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
+    </html>
+  );
+};
+
+export default Layout;
